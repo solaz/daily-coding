998d21124372b97918165154e5e6f7c0d4a79bdf
solaz <moonkyu.kim@gmail.com>
2018-01-03 09:50:05 +0900

    update code, finish code, but there are 100% fatal error. need to investigate.

--- heap.cpp
+++ heap.cpp
@@ -1,86 +1,124 @@
 // Baejun online judge #1927
 #include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
 
-static const int MAX_HEAP_SIZE = 1000;
-class Heap {
-public:
-	Heap() {};
-	~Heap() {};
-	virtual int insertValue(int _value) = 0;
-	virtual int popRootValue() = 0;
-
-protected:
-	int HeapArray[MAX_HEAP_SIZE];
-	int cntElement;
-	int getLeft(int _node_id);
-	int getRight(int _node_id);
-	int getParent(int _node_id);
-};
-
-Heap::Heap() {
-	cntElement = 0;
-	HeapArray[MAX_HEAP_SIZE] = { -1, };
-}
-
-class MinHeap :Heap {
-public:
-	int insertValue(int _val);
-	int popRootValue();
-};
-
-int MinHeap::insertValue(int _val) {
-	int res = 0; int temp = 0;
-	this->HeapArray[this->cntElement] = _val;
-	int checkNode = cntElement;
-	while (1) {
-		if (HeapArray[checkNode/2] > HeapArray[checkNode]) {
-			temp = HeapArray[checkNode/2];
-			HeapArray[checkNode/2] = HeapArray[checkNode];
-			HeapArray[checkNode] = temp;
-		}
-		if (checkNode/2 == 0) break;
-		checkNode /= 2;
-		// TODO: if max depth exists, add escape condition and return -1
-	}
-
-	return res;
-}
-
+/* TODO
+코딩은 마쳤음
+그런데 0, 1이 출력된 후 FC가 발생하여 종료됨
+인덱스 처리에 문제가 있어 보임. 디버깅 중
+*/
 int min(int a, int b) {
 	if (a<b) return a;
 	else return b;
 }
 
-int MinHeap::popRootValue() {
-	int res = HeapArray[0]; int temp = 0;
-	HeapArray[0] = HeapArray[cntElement];
-	int parentId = 0; int childId = 0;
+static const int MAX_HEAP_SIZE = 1000;
+class Heap {
+public:
+	Heap();
+	~Heap() {};
+	virtual int insertValue(int _value) = 0;
+	virtual int popRootValue() = 0;
+
+protected:
+	int mHeapArray[MAX_HEAP_SIZE] = { 0 };
+	int mElementCnt;	// indicate next position to insert
+};
+
+Heap::Heap() {
+	mElementCnt = 1;
+	mHeapArray[MAX_HEAP_SIZE] ;
+}
+
+class MinHeap : Heap {
+public:
+	MinHeap() {};
+	~MinHeap() {};
+	int insertValue(int _val);
+	int popRootValue();
+};
+
+int MinHeap::insertValue(int _val) {
+	int temp; int childNodeId; int parentNodeId;
+
+	if (mElementCnt > MAX_HEAP_SIZE) return -1;
+
+	mHeapArray[mElementCnt] = _val;
+	if (mElementCnt == 1) {
+		mElementCnt++;
+		return 1;
+	}
+	
+	childNodeId = mElementCnt;
 	while (1) {
-		if ((HeapArray[parentId*2] > 0 && cntElement >= parentId*2)
-			&& (HeapArray[parentId*2+1] > 0 && cntElement >= parentId*2+1)) {
-			childId = min(parentId*2, parentId*2+1);
+		parentNodeId = childNodeId/2;
+		if (mHeapArray[parentNodeId] > mHeapArray[childNodeId]) {
+			temp = mHeapArray[parentNodeId];
+			mHeapArray[parentNodeId] = mHeapArray[childNodeId];
+			mHeapArray[childNodeId] = temp;
 		}
-		else if (HeapArray[parentId*2] > 0 && cntElement >= parentId*2) {
-			childId = parentId*2;
+		if (parentNodeId == 1) break;
+		childNodeId /= 2;
+	}
+	mElementCnt++;
+	return 1;
+}
+
+int MinHeap::popRootValue() {
+	int res = mHeapArray[1]; int temp = 0;
+	int parentNodeId; int leftChildNodeId; int rightChildNodeId; int childNodeIdToSwitch;
+	int lastNodeId;
+
+	if (res == 0) return 0;
+	if (mElementCnt == 1) return res;
+	else if (mElementCnt < 1) {
+		printf("ERROR:1\n");
+		return 0;	// error case
+	}
+
+	lastNodeId = --mElementCnt;
+	mHeapArray[1] = mHeapArray[lastNodeId];
+	mHeapArray[lastNodeId] = 0;
+	
+	parentNodeId = mHeapArray[1];
+
+	while (1) {
+		leftChildNodeId = parentNodeId*2; rightChildNodeId = parentNodeId*2+1;
+		if ((mHeapArray[leftChildNodeId] > 0 && mHeapArray[parentNodeId] < mHeapArray[leftChildNodeId])
+			&& (mHeapArray[rightChildNodeId] > 0 && mHeapArray[parentNodeId] < mHeapArray[rightChildNodeId])) {
+			childNodeIdToSwitch = min(parentNodeId*2, parentNodeId*2+1);
 		}
-		else if (HeapArray[parentId*2+1] > 0 && cntElement >= parentId*2+1) {
-			childId = parentId*2+1;
+		else if (mHeapArray[leftChildNodeId] > 0 && mHeapArray[parentNodeId] < mHeapArray[leftChildNodeId]) {
+			childNodeIdToSwitch = leftChildNodeId;
+		}
+		else if (mHeapArray[rightChildNodeId] > 0 && mHeapArray[parentNodeId] < mHeapArray[rightChildNodeId]) {
+			childNodeIdToSwitch = rightChildNodeId;
 		}
 		else break;
 
-		if (HeapArray[parentId]>HeapArray[childId]) {
-			temp = HeapArray[parentId];
-			HeapArray[parentId] = HeapArray[childId];
-			HeapArray[childId] = temp;
-			parentId = childId;
-		}
+		temp = mHeapArray[parentNodeId];
+		mHeapArray[parentNodeId] = mHeapArray[childNodeIdToSwitch];
+		mHeapArray[childNodeIdToSwitch] = temp;
+		parentNodeId = childNodeIdToSwitch;
 	}
 	return res;
 }
 
 int main()
 {
+	int cnt = 0; int i = 0; int input = 0; int output = 0;
 	freopen("heap_array_input.txt", "r", stdin);
+	scanf("%d", &cnt);
+
+	MinHeap mh;
+	for (i = 0; i<cnt; i++) {
+		scanf("%d", &input);
+		if (input != 0) mh.insertValue(input);
+		else {
+			output = mh.popRootValue();
+			printf("%d\n", output!=0?output:0);
+		}
+	}
 
 	return 0;
 }
